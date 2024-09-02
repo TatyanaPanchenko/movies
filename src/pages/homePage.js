bf76f2d9493b1e5a42306ea4f7c81c96d305d34f
TatyanaPanchenko <tatyanapanchenko.web@gmail.com>
2024-09-02 19:02:12 +0300

    Change project

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,7 +1,5 @@
 import { app, moviesLink, homeLink } from "../vars";
 export default function homePage() {
-  moviesLink.classList.remove("underline");
-  homeLink.classList.add("underline");
   app.innerHTML = `
 	<h1 style="color:#fff">Home</h1>
 		`;
